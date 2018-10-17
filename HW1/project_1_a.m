4f5b209a161e005627aeb0de23fbc24876fb45e5
Chris Georgakidis <chris.georgakidis@gmail.com>
2018-10-17 10:33:59 +0300

    Part A
    In part A I implemented a discrete BPSK with OVERSAMPLE = 1. First, I generate a vector of 0, 1 with equal probability.  Then this input vector is modulated 0 -> -1, 1 -> 1.  Then I generate the symbols for this vector. Since it is BPSK, the symbols will have  its Im = 0. Then for each value of SNR (in dB), I calculate the output of the channel using the Matlab's function awgn, which adds to the input signal an additive white gaussian noise. The next step is to decide whether the output bit is 0 or 1 and after that check if it has error. At the end I calculate the BER and finally graph the result BER vs BER-theory

---
+++ HW1/project_1_a.m
@@ -0,0 +1,63 @@
+%*************** Advanced Communication Systems*****************%
+%                       CE542, Fall 2018                        %
+%                       ECE, UTH, Greece                        %
+% File: project_1                                               %
+% Authors: Christos Georgakidis (1964)                          %
+%***************************************************************%
+
+% pkg load communications
+clear all
+close all
+
+% M = 2 % BPSK %
+SNRdB = 0:20; % SNR in dB %
+N = 100000; % number of bits %
+
+% System Specifications %
+% O = 1 % Oversampling %
+Eb = 1; % Energy / bit %
+% convert  SNR from db to linear: SNR(dB) = 10log10(SNR(linear)) %
+SNR_linear = 10.^(SNRdB/10);
+nOfIterations = 1;  % num of iterations for Monte Carlo Simulation %
+
+%  Transmitter  %
+bits = rand(1, N) > 0.5; % generating 0, 1 with equal probability %
+s = 2*bits - 1; % BPSK modulation 0 -> -1, 1 -> 1 %
+x = complex(s);
+
+for ii = 1:length(SNRdB)
+    %  Channel  %
+    y = awgn(x, SNRdB(ii));
+
+%     scatterplot(y);
+
+    %  Receiver  %
+    output = real(y)>0; % decision %
+
+    % counting the errors %
+    error_num = 0;
+    for i = 1:N
+        if output(i) ~= bits(i)
+            error_num = error_num + 1;
+        end
+    end
+    BER(ii) = error_num/N; % Bit Error Rate %
+end
+
+BER_exp = (1/2)*erfc(sqrt(SNR_linear));
+
+% BER - SNR Graph %
+figure(1)
+%plot start
+semilogy(SNRdB,BER,'o','linewidth',2.5),grid on,hold on;
+semilogy(SNRdB,BER_exp,'r','linewidth',2.5);
+title(' curve for Bit Error Rate verses  SNR for Binary PSK modulation');
+xlabel(' SNR(dB)');
+ylabel('BER');
+legend('simulation','theorytical');
+axis([0 10 10^-5 1]);
+
+
+
+
+
